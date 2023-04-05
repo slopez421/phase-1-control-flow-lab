635b965f198b9ffa2d77e37faca2efe88bd2fd3b
Shania Lopez <slopez@fandm.edu>
2023-04-04 20:10:50 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+
+  let result
+  if (someValue <= 400) {
+    result = 'This one is on me!';
+  } else if (someValue <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if (someValue <= 2499) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (someValue >= 2500) {
+    result = 'No can do.';
+  }
+
+  return result
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+
+ let result = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+
+  return result;
+  
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+switch (tip) {
+  case 'generous' :
+    return 'Thank you so much.'
+  case 'not as generous' :
+    return 'Thank you.'
+  default: 
+  return 'Bye.'
+}
+
+
 }
